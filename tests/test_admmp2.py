124ef38e45ff37da05596d5143fc8202a0e27d75
Jérémy Jn <74363374+heez77@users.noreply.github.com>
2023-03-03 10:58:09 +0100

    Add test for adjoint

--- tests/test_admmp2.py
+++ tests/test_admmp2.py
@@ -15,33 +15,48 @@ class Test_TestADMMP2(unittest.TestCase):
         y = algo.gaussian_filter_2d()
         self.assertTrue(np.allclose(np.asarray(y.shape), np.asarray(x.shape) - 2))
         
-    def test_precompute_matrix_invTmu(self):
-        #Pas good encore
-        """ Test if all of the points are in the ball"""
-        rng = np.random.default_rng(84548)
-        shape = rng.integers(10,100)
-        x = rng.integers(0, 255, size=(shape, shape))
-        h = h = 1/16 * np.array([[1,2,1],
-                     [2,4,2],
-                     [1,2,1]])
-        
-        algo = ADMMP2(x, h)
-        y =  algo.gaussian_filter_2d()
-        _, inv_Tmu =  algo._precompute_matrix(x, y, 0.3)
-        i, j = inv_Tmu.shape
-        test = inv_Tmu.reshape(-1)[:-1].reshape(i-1, j+1)
-        self.assertTrue(~np.any(test[:, 1:]), True)
-    
     def test_precompute_matrix_padding(self):
         """ Test if all of the points are in the ball"""
         rng = np.random.default_rng(84548)
         shape= rng.integers(10,100)
         x = rng.integers(0, 255, size=(shape, shape))
-        h = h = 1/16 * np.array([[1,2,1],
+        h = 1/16 * np.array([[1,2,1],
                      [2,4,2],
                      [1,2,1]])
         
         algo = ADMMP2(x, h)
         y =  algo.gaussian_filter_2d()
-        pre_comput_Ty, _ =  algo._precompute_matrix(x, y, 0.3)
+        pre_comput_Ty =  algo._precompute_matrix(x, y, 0.3)
         self.assertTrue(np.allclose(np.asarray(pre_comput_Ty.shape), np.asarray(x.shape)), True)
+    
+    def test_adjoint_A(self):
+        rng = np.random.default_rng(84548)
+        shape= rng.integers(10,100) //2 * 2
+        x = rng.integers(0, 255, size=(shape, shape)) / 255.
+        y = rng.integers(0, 255, size=(shape, shape)) / 255.
+        h = 1/16 * np.array([[1,2,1],
+                     [2,4,2],
+                     [1,2,1]])
+        
+        algo = ADMMP2(x, h)
+        Ax = algo.fft_dot(x, algo.A_fft)
+        A_star_y = algo.fft_dot_adj(y, algo.A_fft)
+        dot_a = np.hstack(Ax) @ np.hstack(y)
+        dot_b = np.hstack(x) @ np.hstack(A_star_y)
+        self.assertAlmostEqual(dot_a, dot_b)
+    
+    def test_adjoint_R(self):
+        rng = np.random.default_rng(84548)
+        shape= rng.integers(10,100) //2 * 2
+        x = rng.integers(0, 255, size=(shape, shape)) / 255.
+        h = 1/16 * np.array([[1,2,1],
+                     [2,4,2],
+                     [1,2,1]])
+        
+        algo = ADMMP2(x, h)
+        Rx, slices = algo.wavelet_transform(x)
+        y = rng.integers(0, 255, size=Rx.shape) / 255.
+        R_star_y = algo.wavelet_transform_adjoint(y, slices)
+        dot_a = np.hstack(Rx) @ np.hstack(y)
+        dot_b = np.hstack(x) @ np.hstack(R_star_y)
+        self.assertAlmostEqual(dot_a, dot_b)
